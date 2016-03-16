7f783a87c240609677f35f6192d80c32bd4b98c2
wicak29 <adi.wicaksana13@mhs.if.its.ac.id>
2016-03-16 16:37:40 +0700

    edit base_url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/buku_tamu';
+$config['base_url'] = 'http://10.151.36.5/buku_tamu';
 $config['site_name'] = 'Buku Tamu';
 
 /*
